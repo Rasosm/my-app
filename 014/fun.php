d6eee9b6bf21c6e90f9654d4e4fb8898a852c8a9
Rasosm <rasosm@gmail.com>
2022-12-15 14:30:49 +0200

    Funkcijos

--- 014/fun.php
+++ 014/fun.php
@@ -1,5 +1,7 @@
 <?php
 
+echo'<pre>';
+
 echo '<br>';
 echo '<br>1---------------';
 echo '<br>';
@@ -28,20 +30,61 @@ function giveMeStr2(string $str, int $num): string
 echo giveMeStr2($str, 4);
 
 echo '<br>';
-echo '<br>2---------------';
+echo '<br>3---------------';
+echo '<br>';
+
+echo '<br>';
+echo '<br>4---------------';
 echo '<br>';
 
 function pirminis(int $skaicius): int
 {
   $pirminiaiSum = 0;
-
   for ($i = 2; $i < $skaicius; $i++) {
     if ($skaicius % $i === 0) {
       $pirminiaiSum++;
     }
   }
-
   return $pirminiaiSum;
+
 }
 
-echo pirminis(12);
+echo pirminis(573);
+
+echo '<br>';
+echo '<br>5---------------';
+echo '<br>';
+
+foreach(range(1,10) as $_){
+    $masyvas33[] = rand(33,77);
+}
+echo '<br>';
+ print_r($masyvas33);
+
+ $masyvasBeLiekanos = [];
+
+ foreach ($masyvas33 as $value) {
+    $masyvasBeLiekanos[] = pirminis($value);
+    sort($masyvasBeLiekanos);
+ }
+
+ print_r($masyvasBeLiekanos);
+
+
+ echo '<br>';
+echo '<br>6---------------';
+echo '<br>';
+
+foreach(range(1,20) as $_){
+    $masyvas333[] = rand(333,777);
+}
+echo '<br>';
+ print_r($masyvas333);
+
+ foreach ($masyvas333 as $value1) {
+if(pirminis($value1) !== 0){
+    $masyvasBePirminiu[] = $value1;
+}
+ }
+
+ print_r($masyvasBePirminiu);
